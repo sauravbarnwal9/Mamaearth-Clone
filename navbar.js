96c9d8596c52278ef21e94be47fd3a49904085ab
Amit Singh <amiteee14003@gmail.com>
2022-05-06 16:40:48 +0530

    Made the Navbar

---
+++ navbar.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    headerFun()
+};
+var header = document.querySelector("#sticky-header");
+var sticky = header.offsetTop;
+function headerFun() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
